e5907a5c62f38fbb777036d76c915acb001ab7df
Marta Ginosyan <marta.ginosian@gmail.com>
2016-09-28 18:39:33 +0400

    Implements position updating functionality with validation.

--- src/main/java/validator/ErrorTypes.java
+++ src/main/java/validator/ErrorTypes.java
@@ -5,14 +5,22 @@ package validator;
  */
 public class ErrorTypes {
 
+    // region General
+    static final String NULL_VALUE = "Something went wrong, a null value occur";
+    // endregion
+
     // region New employee name
-    public static String EMPTY_EMPLOYEE_NAME = "No name entered";
-    public static String EMPLOYEE_NAME_EXIST = "Employee already exist";
-    public static String NOT_ALPHABETIC_EMPLOYEE_NAME = "Employee name should consist of alphabetic symbols";
+    static final String EMPTY_EMPLOYEE_NAME = "No name entered";
+    static final String EMPLOYEE_NAME_EXIST = "Employee already exist";
+    static final String NOT_ALPHABETIC_EMPLOYEE_NAME = "Employee name should consist of alphabetic symbols";
     // endregion
 
     // region Employee birthday date
-    public static String INVALID_BIRTHDAY_DATE = "Birthday date should be before current date";
-    public static String EMPTY_BIRTHDAY_DATE = "No birthday date added";
+    static final String INVALID_BIRTHDAY_DATE = "Birthday date should be before current date";
+    static final String EMPTY_BIRTHDAY_DATE = "No birthday date added";
+    // endregion
+
+    // region Employee position
+    static final String EMPTY_POSITION = "The dialog opened to add position so add one";
     // endregion
 }
